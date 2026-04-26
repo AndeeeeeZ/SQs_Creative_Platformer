27e49289048a4921eff9e8d6316f433ef0f205bd
AndeeeeeZ <zhuandeee@gmail.com>
2026-04-25 23:02:30 -0400

    Revised Input System

--- Assets/Scripts/Player/PlayerMarkController.cs
+++ Assets/Scripts/Player/PlayerMarkController.cs
@@ -3,26 +3,31 @@ using UnityEngine;
 public class PlayerMarkController : MonoBehaviour
 {
     private SpriteRenderer sr;
-    private Animator animator; 
+    private Animator animator;
 
     private void Awake()
     {
-        sr = GetComponent<SpriteRenderer>(); 
-        animator = GetComponent<Animator>(); 
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     public void StartWalking()
     {
-        animator.Play("Walk"); 
+        animator.Play("Walk");
     }
 
     public void StopWalking()
     {
-        animator.Play("Idle"); 
+        animator.Play("Idle");
     }
 
     public void SetDirection(float direction)
     {
-        sr.flipX = direction <= 0; 
+        sr.flipX = direction <= 0;
+    }
+
+    public void StartWalkingBack()
+    {
+        animator.Play("WalkBackward"); 
     }
 }
